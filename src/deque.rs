d7d40e5cf1e5cbf8188a13e78903e46bad736635
Stjepan Glavina <stjepang@gmail.com>
2017-03-30 18:11:50 +0200

    Add some deque tests

--- src/deque.rs
+++ src/deque.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::marker::PhantomData;
 use std::mem;
 use std::ptr;
 use std::sync::Arc;
@@ -75,6 +77,7 @@ impl<T> Deque<T> {
         }
     }
 
+    #[cold]
     unsafe fn replace(&self, old: *mut Array<T>, new: Array<T>) -> *mut Array<T> {
         let new = Box::into_raw(Box::new(new));
         self.array.store_raw(new, Release);
@@ -178,36 +181,68 @@ impl<T> Drop for Deque<T> {
     }
 }
 
-// TODO: Sync and Send (use PhantomData!)
-
 pub struct Worker<T> {
     deque: Arc<Deque<T>>,
+    _marker: PhantomData<*mut ()>, // !Send + !Sync
 }
 
+unsafe impl<T: Send> Send for Worker<T> {}
+
 impl<T> Worker<T> {
-    fn push(&self, value: T) {
+    pub fn push(&self, value: T) {
         self.deque.push(value);
     }
 
-    fn pop(&self) -> Option<T> {
+    pub fn pop(&self) -> Option<T> {
         self.deque.pop()
+    }
+}
+
+impl<T> fmt::Debug for Worker<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Worker {{ ... }}")
     }
 }
 
 pub struct Stealer<T> {
     deque: Arc<Deque<T>>,
+    _marker: PhantomData<*mut ()>, // !Send + !Sync
 }
 
+unsafe impl<T: Send> Send for Stealer<T> {}
+unsafe impl<T: Send> Sync for Stealer<T> {}
+
 impl<T> Stealer<T> {
-    fn steal(&self) -> Option<T> {
+    pub fn steal(&self) -> Option<T> {
         self.deque.steal()
+    }
+}
+
+impl<T> Clone for Stealer<T> {
+    fn clone(&self) -> Self {
+        Stealer {
+            deque: self.deque.clone(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> fmt::Debug for Stealer<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Stealer {{ ... }}")
     }
 }
 
 pub fn deque<T>() -> (Worker<T>, Stealer<T>) {
     let d = Arc::new(Deque::new());
-    let worker = Worker { deque: d.clone() };
-    let stealer = Stealer { deque: d };
+    let worker = Worker {
+        deque: d.clone(),
+        _marker: PhantomData,
+    };
+    let stealer = Stealer {
+        deque: d,
+        _marker: PhantomData,
+    };
     (worker, stealer)
 }
 
@@ -215,8 +250,10 @@ pub fn deque<T>() -> (Worker<T>, Stealer<T>) {
 mod tests {
     extern crate rand;
 
+    use std::sync::{Arc, Mutex};
+    use std::sync::atomic::{AtomicBool, AtomicUsize};
+    use std::sync::atomic::Ordering::SeqCst;
     use std::thread;
-    use std::sync::Arc;
 
     use self::rand::Rng;
 
@@ -224,5 +261,243 @@ mod tests {
 
     #[test]
     fn simple() {
+        let (w, s) = deque();
+        assert_eq!(w.pop(), None);
+        assert_eq!(s.steal(), None);
+
+        w.push(1);
+        assert_eq!(w.pop(), Some(1));
+        assert_eq!(w.pop(), None);
+        assert_eq!(s.steal(), None);
+
+        w.push(2);
+        assert_eq!(s.steal(), Some(2));
+        assert_eq!(s.steal(), None);
+        assert_eq!(w.pop(), None);
+    }
+
+    #[test]
+    fn steal_push() {
+        const STEPS: usize = 50_000;
+
+        let (w, s) = deque();
+        let t = thread::spawn(move || {
+            for i in 0..STEPS {
+                loop {
+                    if let Some(v) = s.steal() {
+                        assert_eq!(i, v);
+                        break;
+                    }
+                }
+            }
+        });
+
+        for i in 0..STEPS {
+            w.push(i);
+        }
+        t.join().unwrap();
+    }
+
+    #[test]
+    fn stampede() {
+        const COUNT: usize = 50_000;
+
+        let (w, s) = deque();
+
+        for i in 0..COUNT {
+            w.push(Box::new(i + 1));
+        }
+        let remaining = Arc::new(AtomicUsize::new(COUNT));
+
+        let threads = (0..8).map(|_| {
+            let s = s.clone();
+            let remaining = remaining.clone();
+
+            thread::spawn(move || {
+                let mut last = 0;
+                while remaining.load(SeqCst) > 0 {
+                    if let Some(x) = s.steal() {
+                        assert!(last < *x);
+                        last = *x;
+                        remaining.fetch_sub(1, SeqCst);
+                    }
+                }
+            })
+        }).collect::<Vec<_>>();
+
+        let mut last = COUNT + 1;
+        while remaining.load(SeqCst) > 0 {
+            if let Some(x) = w.pop() {
+                assert!(last > *x);
+                last = *x;
+                remaining.fetch_sub(1, SeqCst);
+            }
+        }
+
+        for t in threads {
+            t.join().unwrap();
+        }
+    }
+
+    #[test]
+    fn stress() {
+        const COUNT: usize = 50_000;
+
+        let (w, s) = deque();
+        let done = Arc::new(AtomicBool::new(false));
+        let hits = Arc::new(AtomicUsize::new(0));
+
+        let threads = (0..8).map(|_| {
+            let s = s.clone();
+            let done = done.clone();
+            let hits = hits.clone();
+
+            thread::spawn(move || {
+                while !done.load(SeqCst) {
+                    if let Some(_) = s.steal() {
+                        hits.fetch_add(1, SeqCst);
+                    }
+                }
+            })
+        }).collect::<Vec<_>>();
+
+        let mut rng = rand::thread_rng();
+        let mut expected = 0;
+        while expected < COUNT {
+            if rng.gen_range(0, 3) == 0 {
+                if w.pop().is_some() {
+                    hits.fetch_add(1, SeqCst);
+                }
+            } else {
+                w.push(expected);
+                expected += 1;
+            }
+        }
+
+        while hits.load(SeqCst) < COUNT {
+            if w.pop().is_some() {
+                hits.fetch_add(1, SeqCst);
+            }
+        }
+        done.store(true, SeqCst);
+
+        for t in threads {
+            t.join().unwrap();
+        }
+    }
+
+    #[test]
+    fn no_starvation() {
+        const COUNT: usize = 50_000;
+
+        let (w, s) = deque();
+        let done = Arc::new(AtomicBool::new(false));
+
+        let (threads, hits): (Vec<_>, Vec<_>) = (0..8).map(|_| {
+            let s = s.clone();
+            let done = done.clone();
+            let hits = Arc::new(AtomicUsize::new(0));
+
+            let t = {
+                let hits = hits.clone();
+                thread::spawn(move || {
+                    while !done.load(SeqCst) {
+                        if let Some(_) = s.steal() {
+                            hits.fetch_add(1, SeqCst);
+                        }
+                    }
+                })
+            };
+
+            (t, hits)
+        }).unzip();
+
+        let mut rng = rand::thread_rng();
+        let mut my_hits = 0;
+        loop {
+            for i in 0..rng.gen_range(0, COUNT) {
+                if rng.gen_range(0, 3) == 0 && my_hits == 0 {
+                    if w.pop().is_some() {
+                        my_hits += 1;
+                    }
+                } else {
+                    w.push(i);
+                }
+            }
+
+            if my_hits > 0 && hits.iter().all(|h| h.load(SeqCst) > 0) {
+                break;
+            }
+        }
+        done.store(true, SeqCst);
+
+        for t in threads {
+            t.join().unwrap();
+        }
+    }
+
+    #[test]
+    fn destructors() {
+        const COUNT: usize = 50_000;
+
+        struct Elem(usize, Arc<Mutex<Vec<usize>>>);
+
+        impl Drop for Elem {
+            fn drop(&mut self) {
+                self.1.lock().unwrap().push(self.0);
+            }
+        }
+
+        let (w, s) = deque();
+
+        let dropped = Arc::new(Mutex::new(Vec::new()));
+        let remaining = Arc::new(AtomicUsize::new(COUNT));
+        for i in 0..COUNT {
+            w.push(Elem(i, dropped.clone()));
+        }
+
+        let threads = (0..8).map(|_| {
+            let s = s.clone();
+            let remaining = remaining.clone();
+
+            thread::spawn(move || {
+                for _ in 0..1000 {
+                    if s.steal().is_some() {
+                        remaining.fetch_sub(1, SeqCst);
+                    }
+                }
+            })
+        }).collect::<Vec<_>>();
+
+        for _ in 0..1000 {
+            if w.pop().is_some() {
+                remaining.fetch_sub(1, SeqCst);
+            }
+        }
+
+        for t in threads {
+            t.join().unwrap();
+        }
+
+        let rem = remaining.load(SeqCst);
+        assert!(rem > 0);
+
+        {
+            let mut v = dropped.lock().unwrap();
+            assert_eq!(v.len(), COUNT - rem);
+            v.clear();
+        }
+
+        drop(w);
+        drop(s);
+
+        {
+            let mut v = dropped.lock().unwrap();
+            assert_eq!(v.len(), rem);
+            v.sort();
+            for w in v.windows(2) {
+                assert_eq!(w[0] + 1, w[1]);
+            }
+        }
     }
 }
