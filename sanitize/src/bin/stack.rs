24a89c2cc1f1bcf5e64bc23b46d740b42d1643b6
Stjepan Glavina <stjepang@gmail.com>
2017-04-08 21:08:37 +0200

    Update clang and llvm

--- sanitize/src/bin/stack.rs
+++ sanitize/src/bin/stack.rs
@@ -2,17 +2,21 @@ extern crate coco;
 extern crate rand;
 
 use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::SeqCst;
 use std::thread;
 
+use coco::Stack;
 use rand::Rng;
-use coco::{epoch, Stack};
 
 fn stress_test() {
-    // TODO
 }
 
 fn leak_test() {
     let s = Arc::new(Stack::new());
+    s.push(10);
+    std::mem::forget(s);
+    return;
 
     let threads = (0..8).map(|t| {
         let s = s.clone();
